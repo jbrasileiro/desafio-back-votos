27aa3ebf8f7d86c73287384e0141163735957080
João Brasileiro <jcbrasileiro@hotmail.com>
2020-05-16 19:53:07 -0300

    add default files

---
+++ src/main/java/com/github/jbrasileiro/dainichi/DainichiVotingApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.jbrasileiro.dainichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DainichiVotingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DainichiVotingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jbrasileiro/dainichi/DainichiVotingApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.jbrasileiro.dainichi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DainichiVotingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//no content
+	}
+
+}
